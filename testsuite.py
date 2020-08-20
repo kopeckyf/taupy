c286f8a92faabe70db61933551484931348ddea3
Felix Kopecky <kopeckyf@users.noreply.github.com>
2020-08-20 17:27:05 +0200

    Add rudimentary tests

---
+++ testsuite.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    from taupy import * 
+    from sympy.abc import a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+    tau1 = Debate((a&b) >> c, (d&e) >> f)
+    pos1 = Position(tau1, {a: True, b: True})
+    pos1.is_closed()

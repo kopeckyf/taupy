a88242a8b256a94b84212c71b1c31672a82b779d
Felix Kopecky <kopeckyf@users.noreply.github.com>
2020-09-10 17:11:31 +0200

    Simulation.premisepool() now allows for variable length of premises

--- taupy/simulation/simulation.py
+++ taupy/simulation/simulation.py
@@ -3,12 +3,15 @@ Basic tools in simulations
 """
 
 from sympy import symbols, Not
-from itertools import combinations
+from itertools import combinations, chain
 
 class Simulation(list):
     
     def __init__(self, sentencepool="p:10"):
-        self.sentencepool = [i for i in symbols(sentencepool)]
+        if sentencepool == "auto":
+            self.sentencepool = [i for i in self.atoms()]
+        else:
+            self.sentencepool = [i for i in symbols(sentencepool)]
         list.__init__(self)
         
     def premisepool(self, r):
@@ -25,12 +28,17 @@ class Simulation(list):
         in need of optimisation.
         """
         _premisepool = self.sentencepool + [Not(i) for i in self.sentencepool]
-        for i in combinations ( _premisepool , r ):
+        
+        try: # Assume variable length of subsequence. Following an idea by Dan H.
+            _iterator = chain( *map (lambda i: combinations( _premisepool, i ),
+                                     r ) )
+        except TypeError: # the input r is not an iterable. Now assume integer.
+            _iterator = combinations ( _premisepool , r )
+    
+        for i in _iterator:
             if i in self[-1].list_of_premises():
                 # There's already an argument with premises 
                 continue
             for x in i:
                 if Not(x) in i: break
             else: yield i
-    
-        

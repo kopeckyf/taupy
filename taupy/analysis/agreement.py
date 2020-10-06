9dba6b8800e5b1421c45a515ef3ee4b506f57ca0
Felix Kopecky <kopeckyf@users.noreply.github.com>
2020-10-06 21:09:40 +0200

    More robust hd() For this version of hd(), it is irrelavant whether the positions assert the same statements. The only test is whether their size equals. This is one of the possible interpretations of transferring the Hamming distance from ordered sequences (like strings) to mappings. The other interpretation would require that the compared positions give statements on exactly the same propositions, differing only in truth value attributions

--- taupy/analysis/agreement.py
+++ taupy/analysis/agreement.py
@@ -8,7 +8,7 @@ def hd(pos1, pos2):
     if len(pos1) != len(pos2):
         raise ValueError("Hamming distance is defined for positions of equal length only")
     
-    return len([k for k in pos1 if k in pos2 and pos1[k] != pos2[k]])
+    return len(pos1) - len([k for k in pos1 if k in pos2 and pos1[k] == pos2[k]])
 
 def bna(pos1, pos2):
     """

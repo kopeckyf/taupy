29ab72996c1d9fe70241863173b73319b450461a
Felix Kopecky <kopeckyf@users.noreply.github.com>
2020-10-06 21:34:40 +0200

    Add edit_distance()

--- taupy/analysis/__init__.py
+++ taupy/analysis/__init__.py
@@ -1,7 +1,7 @@
 from .doj import doj
-from .agreement import (hd, bna, next_neighbours)
+from .agreement import (hd, bna, next_neighbours, edit_distance)
 
 __all__ = [
             'doj',
-            'hd', 'bna', 'next_neighbours'
+            'hd', 'bna', 'next_neighbours', 'edit_distance'
           ]
--- taupy/analysis/agreement.py
+++ taupy/analysis/agreement.py
@@ -10,6 +10,44 @@ def hd(pos1, pos2):
     
     return len(pos1) - len([k for k in pos1 if k in pos2 and pos1[k] == pos2[k]])
 
+def edit_distance(pos1, pos2):
+    """
+    A generalised distance measure that does not require the positions to be
+    of equal length. Compared to edit distances for ordered sequences (e.g. 
+    Leventsthein distance), it is far easier to compare two iterables. 
+    
+    For each item, two positions can have four states:
+     - They agree on the item, which does not increase the distance
+     
+    There are three operations that do increase the distance:
+     - Substitution:
+       The positions are equal after transposition, i.e. changing a truth-value
+     - Insertion:
+       One position does not make a statement concerning one proposition. Adding
+       the respective truth-value attribution makes the two positions equal w.r.t
+       the statement.
+     - Deletion:
+       One position does make a statement that the other does not care about. 
+       The positions can be made equal if the first position forgets its statement.
+    
+    In the following measure, these operations are all weighted with factor 1. 
+    There are different interpretations whether, e.g., substition would be weighted
+    at 0.5 and the other two at 1.
+    """
+    
+    #_delta = 0
+    #for v in pos1.keys() | pos2.keys():
+        #if v in pos1 and v in pos2 and pos1[v] == pos2[v]:
+            #pass
+        #else:
+            #_delta += 1
+    #return _delta    
+
+    return len([v for v in pos1.keys() | pos2.keys() if not (
+        v in pos1 and 
+        v in pos2 and 
+        pos1[v] == pos2[v])])
+    
 def bna(pos1, pos2):
     """
     General version of Betz' normalised agreement, which is the Hamming 

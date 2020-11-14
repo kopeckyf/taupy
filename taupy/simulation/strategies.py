25274b9277797ec346f99a656a6c66d0779a30ce
Felix Kopecky <kopeckyf@users.noreply.github.com>
2020-11-14 21:19:24 +0100

    Revamp introduction strategies; add random strategy

--- taupy/simulation/strategies.py
+++ taupy/simulation/strategies.py
@@ -2,26 +2,37 @@
 Argument input strategies, defined in dictionary mappings
 """
 
+random = {"source": False,
+          "target": False,
+          "pick_premises_from": None,
+          "pick_conclusion_from": None,
+          "source_accepts_conclusion": "NA",
+          "target_accepts_conclusion": "NA"}
+
 fortify = {"source": True,
            "target": False,
-           "premises_from": "source",
-           "conclusion_from": "source",
-           "target_accepts_conclusion": "Irrelevant"}
+           "pick_premises_from": "source",
+           "pick_conclusion_from": "source",
+           "source_accepts_conclusion": True,
+           "target_accepts_conclusion": "NA"}
 
 attack = {"source": True,
           "target": True,
-          "premises_from": "source",
-          "conclusion_from": "target",
-          "target_accepts_conclusion": "No"}
+          "pick_premises_from": "source",
+          "pick_conclusion_from": "target",
+          "source_accepts_conclusion": "Toleration",
+          "target_accepts_conclusion": False}
 
 convert = {"source": True,
            "target": True,
-           "premises_from": "target",
-           "conclusions_from": "source",
-           "target_accepts_conclusion": "Yes"}
+           "pick_premises_from": "target",
+           "pick_conclusions_from": "source",
+           "source_accepts_conclusion": True,
+           "target_accepts_conclusion": "NA"}
 
 undercut = {"source": True,
             "target": True,
-            "premises_from": "target",
-            "conclusions_from": "source",
-            "target_accepts_conclusion": "No"}
+            "pick_premises_from": "target",
+            "pick_conclusions_from": None,
+            "source_accepts_conclusion": "Toleration",
+            "target_accepts_conclusion": False}

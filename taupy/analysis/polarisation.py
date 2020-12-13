a0b9e8f62df7150adb5e29d819de226ed0ca71cf
Felix Kopecky <kopeckyf@users.noreply.github.com>
2020-12-13 15:32:21 +0100

    initial commit for submodule analysis.polarisation

---
+++ taupy/analysis/polarisation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def difference_matrix(positions, measure):
+    """
+    Create a difference profile for the given positions relative to a measure.
+    """
+    return np.array([[measure(i, j) for j in positions] for i in positions])
+
+def spread(positions, measure):
+    """
+    The simplest distance measure consists in measuring the diameter of 
+    positions.
+
+    So this is simply the maximum value in the distance matrix.
+    """
+    return np.amax(difference_matrix(positions, measure))

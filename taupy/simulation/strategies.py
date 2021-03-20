edfbe150fdf3601b3dcfba1972c335e865fe4cd1
Felix Kopecky <kopeckyf@users.noreply.github.com>
2021-03-20 10:55:54 +0100

    Improve docstring in the simulation.strategies module

--- taupy/simulation/strategies.py
+++ taupy/simulation/strategies.py
@@ -1,11 +1,29 @@
 """
-Argument input strategies, defined in dictionary mappings. These strategies are
-defined in Betz 2013: Table 6.1.
+The following argumentation strategies are pre-defined in taupy. They all
+introduce arguments that are valid given the current debate stage.
 
-References
-----------
-Betz, Gregor. 2013. Debate dynamics: How controversy improves our beliefs. 
-Springer. DOI: 10/d3cx
+:py:obj:`random`
+    A completely random strategy that works even if a simulation has no
+    positions at all.
+
+:py:obj:`fortify`
+    Insert a valid argument the premises and conclusion of which are accepted
+    by the source position.
+
+:py:obj:`attack`
+    A valid argument. The premises are accepted by the source position, and
+    the source at least tolerates the conclusion. However, the target denies 
+    the conclusion, given its current truth-value attribution.
+
+:py:obj:`convert`
+    A valid argument with premises picked from the target. The conclusion is 
+    picked from the source, and the source also accepts the conclusion. It is
+    not checked whether the target accepts the conclusion.
+
+:py:obj:`undercut`
+    A valid argument is constructed with premises that the target accepts. The
+    source at least tolerates the conclusion. The conclusion however is not 
+    accepted by the target.
 """
 
 random = {"source": False,

2124d88a2bb8e9aad988bb397ab5a3d6c2e70f8f
Felix Kopecky <kopeckyf@users.noreply.github.com>
2021-04-06 23:37:31 +0200

    Add common recipes to simulation.evaluation

--- taupy/simulation/__init__.py
+++ taupy/simulation/__init__.py
@@ -1,6 +1,6 @@
 from .simulation import Simulation, experiment
 from .update import (introduce, response)
-from .evaluation import evaluate_experiment_with_leiden
+from .evaluation import evaluate_experiment
 
 from .strategies import (random, attack, fortify, convert, undercut)
 
@@ -8,6 +8,6 @@ __all__ = [
             'Simulation', 'experiment',
             'introduce',
             'response',
-            'evaluate_experiment_with_leiden',
+            'evaluate_experiment',
             'random', 'attack', 'fortify', 'convert', 'undercut'
           ]
--- taupy/simulation/evaluation.py
+++ taupy/simulation/evaluation.py
@@ -1,28 +1,80 @@
 from igraph import Graph, ADJ_MAX
-from taupy import difference_matrix, group_divergence, normalised_hamming_distance
+from sklearn.cluster import AffinityPropagation, AgglomerativeClustering
+from concurrent.futures import ProcessPoolExecutor
+from taupy import (difference_matrix, group_divergence, normalised_hamming_distance, 
+                   pairwise_dispersion, number_of_groups)
 import numpy as np
+import pandas as pd
 
-def evaluate_experiment_with_leiden(experiment, *, densities=True):
+def evaluate_experiment(experiment, *, function=None, densities=True, executor={}):
+    with ProcessPoolExecutor(**executor) as executor:
+        results = [executor.submit(function, simulation=i) for i in experiment]
+    
+    return pd.concat([i.result() for i in results])
+
+def variance_dispersion(simulation, *, densities=True):
+    if densities:
+        densities = [i.density() for i in simulation]
+
+    dispersions = [pairwise_dispersion(i, measure=normalised_hamming_distance) for i in simulation.positions]
+    
+    if dispersions:
+        return pd.DataFrame(list(zip(densities, dispersions)), columns=["density", "dispersion"])
+    else:
+        return dispersions
+
+def group_divergence_leiden(simulation, *, densities=True):
+    if densities:
+        densities = [i.density() for i in simulation]
+
+    matrices = [difference_matrix(i, measure=normalised_hamming_distance) for i in simulation.positions]
+    filtered_matrices = [np.exp(-4 * i.astype("float64")) for i in matrices]
+    
+    for i in filtered_matrices:
+        np.fill_diagonal(i, 0)
+        # Assume number of positions is homogenous.
+        i[np.triu_indices(len(simulation.positions[0]))] = 0
+        i[i<0.2] = 0
+    
+    graphs = [Graph.Weighted_Adjacency(i.astype("float64").tolist(), mode=ADJ_MAX) for i in filtered_matrices]
+    clusterings = [list(g.community_leiden(weights="weight", objective_function="modularity")) for g in graphs]
+    divergences = [group_divergence(i, matrices[num]) for num, i in enumerate(clusterings)]
 
     if densities:
-        densities = [[i.density() for i in j] for j in experiment]
+        return pd.DataFrame(list(zip(densities, divergences)), columns=["density", "divergence"])
+    else:
+        return divergences
 
-    matrices = [[difference_matrix(i, measure=normalised_hamming_distance) for i in j.positions] for j in experiment]
-    filtered_matrices = [[np.exp(-4 * i.astype("float64")) for i in m] for m in matrices]
+def group_divergence_agglomerative(simulation, *, densities=True):
+    if densities:
+        densities = [i.density() for i in simulation]
+
+    matrices = [difference_matrix(i, measure=normalised_hamming_distance) for i in simulation.positions]
+    agglomerative = [AgglomerativeClustering(affinity="precomputed", 
+                                             n_clusters=None, 
+                                             compute_full_tree=True, 
+                                             distance_threshold=.75, 
+                                             linkage="complete").fit(i) for i in matrices]
     
-    for j in filtered_matrices:
-        for i in j:
-            np.fill_diagonal(i, 0)
-            # Assume number of positions is homogenous in the complete
-            # experiment.
-            i[np.triu_indices(len(experiment[0].positions[0]))] = 0
-            i[i<0.2] = 0
-    
-    graphs = [[Graph.Weighted_Adjacency(i.astype("float64").tolist(), mode=ADJ_MAX) for i in j] for j in filtered_matrices]
-    clusterings = [[list(i.community_leiden(weights="weight", objective_function="modularity")) for i in g] for g in graphs]
-    divergences = [[group_divergence(i, matrices[mnum][num]) for num, i in enumerate(j)] for mnum, j in enumerate(clusterings)]
+    clusters = [[[i[0] for i in enumerate(k.labels_) if i[1] == j] for j in range(k.n_clusters_)] for k in agglomerative]
+    divergences = [group_divergence(i, matrices[num]) for num, i in enumerate(clusters)]
 
     if densities:
-        return (densities, divergences)
+        return pd.DataFrame(list(zip(densities, divergences)), columns=["density", "divergence"])
+    else:
+        return divergences
+
+
+def group_divergence_affinity_propagation(simulation, *, densities=True):
+    if densities:
+        densities = [i.density() for i in simulation]
+    
+    matrices = [-1 * difference_matrix(i, measure=normalised_hamming_distance) for i in simulation.positions]
+    fits = [AffinityPropagation(affinity="precomputed", random_state=0).fit(i) for i in matrices]
+    clusterings = [[[i[0] for i in enumerate(k.labels_) if i[1] == j] for j in range(len(k.cluster_centers_indices_))] for k in fits]
+    divergences = [group_divergence(i, matrices[num]) for num, i in enumerate(clusterings)]
+
+    if densities:
+        return pd.DataFrame(list(zip(densities, divergences)), columns=["density", "divergence"])
     else:
         return divergences

42cc13a55015a85ed967b4347b39feea789364e2
Felix Kopecky <kopeckyf@users.noreply.github.com>
2021-07-03 12:24:26 +0200

    Add setup files

--- setup.py
+++ setup.py
@@ -1,25 +1,85 @@
-import setuptools
+"""A Python package to study the theory of dialectical structures
+"""
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
 
-setuptools.setup(
-    name="taupy",
-    version="0.0.1",
-    author="Felix Kopecky",
-    description="A Python package to study the theory of dialectical structures",
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='taupy',
+    version='0.1.0',
+    description='A Python package to study the theory of dialectical \
+                 structures',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/kopeckyf/taupy",
-    project_urls={
-        "Bug Tracker": "https://github.com/kopeckyf/taupy/issues",
-    },
+    long_description_content_type='text/markdown', 
+    url='https://github.com/kopeckyf/taupy',
+    author='Felix Kopecky',
+    author_email='f.kopecky@kit.edu',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD-3 License",
-        "Operating System :: OS Independent",
+        'Development Status :: 5 - Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Sociology',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: BSD-3 License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
     ],
-    package_dir={"": "taupy"},
-    packages=setuptools.find_packages(where="taupy"),
-    python_requires=">=3.9",
+
+    keywords='Argumentation theory, Theory of dialectical structures, \
+              Argumentation framework, Agent-based debate model, ABM',
+
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    package_dir={'': './'},
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(where='./'),
+    python_requires='>=3.9, <4',
+    install_requires=['sympy>=1.6.2',
+                      'dd>=0.5.6',
+                      'numpy>=1.19.4',
+                      'python_igraph>=0.9.6',
+                      'scikit_learn>=0.24.2',
+                      'pandas>=1.2.2'],
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={
+        'Source': 'https://github.com/kopeckyf/taupy/',
+        'Bug Reports': 'https://github.com/kopeckyf/taupy/issues',
+        'Citation information': 'https://github.com/kopeckyf/taupy/',
+        'More info about scientific background': 'https://debatelab.philosophie.kit.edu/',
+    },
 )

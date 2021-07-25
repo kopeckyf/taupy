f00b86e7f849ad4c1e07e4c3412fbc6f44ea0dcc
Felix Kopecky <kopeckyf@users.noreply.github.com>
2021-07-25 17:29:23 +0200

    Module simulation.evaluation: Add recipes for polarisation analysis in simulations with partial positions
     - Use normalised_edit_distance()
     - Less scaling and filtering appears to be needed for partial positions

--- taupy/simulation/evaluation.py
+++ taupy/simulation/evaluation.py
@@ -24,11 +24,11 @@ def mean_population_wide_agreement(simulation, *, densities=True):
     else:
         return agreement
 
-def variance_dispersion(simulation, *, densities=True):
+def variance_dispersion(simulation, *, measure=normalised_hamming_distance, densities=True):
     if densities:
         densities = [i.density() for i in simulation]
 
-    dispersions = [pairwise_dispersion(i, measure=normalised_hamming_distance) for i in simulation.positions]
+    dispersions = [pairwise_dispersion(i, measure=measure) for i in simulation.positions]
     
     if densities:
         return pd.DataFrame(list(zip(densities, dispersions)), columns=["density", "dispersion"])
@@ -84,6 +84,26 @@ def group_measures_agglomerative(simulation, *, densities=True):
     else:
         return divergences
 
+def group_measures_leiden_partial_positions(simulation, *, densities=True):
+    if densities:
+        densities = [i.density() for i in simulation]
+
+    matrices = [difference_matrix(i, measure=normalised_edit_distance) for i in simulation.positions]
+    filtered_matrices = [np.exp(-3 * i.astype("float64")) for i in matrices]
+    
+    graphs = [Graph.Weighted_Adjacency(i.astype("float64").tolist(), mode=ADJ_MAX) for i in filtered_matrices]
+    clusterings = [list(g.community_leiden(weights="weight", objective_function="modularity")) for g in graphs]
+    divergences = [group_divergence(i, matrices[num]) for num, i in enumerate(clusterings)]
+    consensus = [group_consensus(i, matrices[num]) for num, i in enumerate(clusterings)]
+    numbers = [number_of_groups(i) for i in clusterings]
+    size_parity = [group_size_parity(i) for i in clusterings]
+
+    if densities:
+        return pd.DataFrame(list(zip(densities, divergences, consensus, numbers, size_parity)), 
+                            columns=["density", "divergence", "consensus", "numbers", "size_parity"])
+    else:
+        return divergences
+
 
 def group_measures_affinity_propagation(simulation, *, densities=True):
     if densities:
@@ -97,6 +117,26 @@ def group_measures_affinity_propagation(simulation, *, densities=True):
         # Assume number of positions is homogenous.
         i[np.triu_indices(len(simulation.positions[0]))] = 0
         i[i<0.2] = 0
+
+    fits = [AffinityPropagation(affinity="precomputed", random_state=0).fit(i) for i in filtered_matrices]
+    clusterings = [[[i[0] for i in enumerate(k.labels_) if i[1] == j] for j in range(len(k.cluster_centers_indices_))] for k in fits]
+    divergences = [group_divergence(i, matrices[num]) for num, i in enumerate(clusterings)]
+    consensus = [group_consensus(i, matrices[num]) for num, i in enumerate(clusterings)]
+    numbers = [number_of_groups(i) for i in clusterings]
+    #size_parity = [group_size_parity(i) for i in clusterings]
+
+    if densities:
+        return pd.DataFrame(list(zip(densities, divergences, consensus, numbers)), 
+                            columns=["density", "divergence", "consensus", "numbers"])
+    else:
+        return divergences
+
+def group_measures_affinity_propagation_partial_positions(simulation, *, densities=True):
+    if densities:
+        densities = [i.density() for i in simulation]
+    
+    matrices = [difference_matrix(i, measure=normalised_edit_distance) for i in simulation.positions]
+    filtered_matrices = [np.exp(-3 * i.astype("float64")) for i in matrices]
 
     fits = [AffinityPropagation(affinity="precomputed", random_state=0).fit(i) for i in filtered_matrices]
     clusterings = [[[i[0] for i in enumerate(k.labels_) if i[1] == j] for j in range(len(k.cluster_centers_indices_))] for k in fits]

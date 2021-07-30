58363ee10506241c62f8481023176cf6d97a0374
Felix Kopecky <kopeckyf@users.noreply.github.com>
2021-07-30 14:46:26 +0200

    Support for multiple events! Added event `new_sentence`, which extends the sentence pool.
     - Simulation.events is a mapping of events {introduction, new_sentence}
       to weights. At each iteration of a simulation, an event is selected
       based on the event's weight given as the mapping's value.
    
     - Implemented event `new_sentence`, which adds a proposition to the
       sentencepool if max_sentencepool is not reached, and it automatically
       has the positions react to the introduction. They react by either
       suspending judgement or passing True or False to the new sentence, each
       with a chance of 1/3.

--- taupy/simulation/simulation.py
+++ taupy/simulation/simulation.py
@@ -2,7 +2,7 @@
 Basic tools in simulations
 """
 from sympy import symbols, Not
-from random import choice, sample
+from random import choice, choices, sample
 from copy import deepcopy
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import time
@@ -16,7 +16,9 @@ class Simulation(list):
     
     def __init__(self,
                  directed = True, 
+                 events = {"introduction": 9, "new_sentence": 1},
                  sentencepool = "p:10",
+                 max_sentencepool = None,
                  leaves = [],
                  sinks = [],
                  parent_debate = None,
@@ -33,6 +35,10 @@ class Simulation(list):
         else:
             self.sentencepool = [i for i in symbols(sentencepool)]
             self.used_premises = []
+
+        self.max_sentencepool = [i for i in symbols(max_sentencepool)] if max_sentencepool else self.sentencepool
+
+        self.events = events
 
         self.argumentlength = argumentlength
         self.leaves = [i for i in symbols(leaves)]
@@ -68,7 +74,7 @@ class Simulation(list):
             target_length = len(self.sentencepool)
 
         for p in positions:
-            # This can generate a problem if the user has supplied positions to begin with.
+            # TODO: This can generate a problem if the user has supplied positions to begin with.
             pool = sample(self.sentencepool, k=target_length)
             for s in pool:
                 if s not in p:
@@ -90,64 +96,106 @@ class Simulation(list):
         i = 0
 
         while True:
-            if self.directed and len(self.positions[-1]) >= 2:
-                # The user asked for a directed simulation and has supplied
-                # enough Positions. Now instantiate turns of trying to introduce
-                # arguments.
-                j = 0
-                while j < len(self.positions[-1]) / 2:
-                    pick_positions = sample(self.positions[-1], k=2)
-                    
-                    # Support for positions with multiple introduction strategies. 
-                    # First, try to pick a random element from the list of introduction
-                    # strategies of a position. If that fails, assume that the strategy
-                    # preference of a position is not given as a list, but as a single
-                    # item.
-                    try:
-                        pick_strategy = choice(pick_positions[0].introduction_strategy)
-                    except KeyError:
-                        pick_strategy = pick_positions[0].introduction_strategy
-                    
-                    argument_introduced = introduce(self,
-                                                    source=pick_positions[0],
-                                                    target=pick_positions[1],
-                                                    strategy=pick_strategy)
-                    if not argument_introduced:
-                        # The argument introduction did not succeed in this turn.
-                        # We are trying it again if there have been less tries than
-                        # half the size of the population.
-                        j += 1
-                        continue
-                    else:
-                        # An argument was found, break out of the loop. 
-                        # (implies argument_introduced == True)
-                        self.log.append("Argument introduction suceeded after %d attempts." % (j+1))
-                        break
-                else:
-                    # There have been more tries equal to half of the population
-                    # size, but an argument could not be found. This is enough
-                    # grounds to terminate the simulation run.
-                    self.log.append("Argument introduction did not succeed, even after %d attempts." % (j+1))
-                    argument_introduced = False
-                    
-                if argument_introduced:
-                    # Check if introduction was succesful before attempting response.
-                    response(self, method=self.default_update_strategy)
-            else:
-                # The user did not ask for a directed simulation and/or provided less than 2 positions.
-                # In this case, we're investing less work.
-                argument_introduced = introduce(self, strategy=self.default_introduction_strategy)
-                if argument_introduced:
-                    response(self, method=self.default_update_strategy)
+
+            selected_event = choices([i[0] for i in self.events.items()],
+                                     weights=[i[1] for i in self.events.items()])[0]
             
-            if not argument_introduced:
-                # Break out of the Simulation if no argument could be inserted.
-                # In this case, the log will tell more about what went wrong.
-                break
+            if selected_event not in ["introduction", "new_sentence"]:
+                raise NotImplementedError(f"No recipe for event type {selected_event}.")
+
+            if selected_event == "introduction":
+                if self.directed and len(self.positions[-1]) >= 2:
+                    # The user asked for a directed simulation and has supplied
+                    # enough Positions. Now instantiate turns of trying to introduce
+                    # arguments.
+                    j = 0
+                    while j < len(self.positions[-1]) / 2:
+                        pick_positions = sample(self.positions[-1], k=2)
+                        
+                        # Support for positions with multiple introduction strategies. 
+                        # First, try to pick a random element from the list of introduction
+                        # strategies of a position. If that fails, assume that the strategy
+                        # preference of a position is not given as a list, but as a single
+                        # item.
+                        try:
+                            pick_strategy = choice(pick_positions[0].introduction_strategy)
+                        except KeyError:
+                            pick_strategy = pick_positions[0].introduction_strategy
+                        
+                        argument_introduced = introduce(self,
+                                                        source=pick_positions[0],
+                                                        target=pick_positions[1],
+                                                        strategy=pick_strategy)
+                        if not argument_introduced:
+                            # The argument introduction did not succeed in this turn.
+                            # We are trying it again if there have been less tries than
+                            # half the size of the population.
+                            j += 1
+                            continue
+                        else:
+                            # An argument was found, break out of the loop. 
+                            # (implies argument_introduced == True)
+                            self.log.append("Argument introduction suceeded after %d attempts." % (j+1))
+                            break
+                    else:
+                        # There have been more tries equal to half of the population
+                        # size, but an argument could not be found. This is enough
+                        # grounds to terminate the simulation run.
+                        self.log.append("Argument introduction did not succeed, even after %d attempts." % (j+1))
+                        argument_introduced = False
+                        
+                    if argument_introduced:
+                        # Check if introduction was succesful before attempting response.
+                        response(self, method=self.default_update_strategy)
+                else:
+                    # The user did not ask for a directed simulation and/or provided less than 2 positions.
+                    # In this case, we're investing less work.
+                    argument_introduced = introduce(self, strategy=self.default_introduction_strategy)
+                    if argument_introduced:
+                        response(self, method=self.default_update_strategy)
+                
+                if not argument_introduced:
+                    # Break out of the Simulation if no argument could be inserted.
+                    # In this case, the log will tell more about what went wrong.
+                    break
+            
+            if selected_event == "new_sentence":
+                # Let's see which sentences could be inserted into the debate.
+                # This will be the difference between the sentencepool and the max_sentencepool
+
+                sentence_candidates = set(self.max_sentencepool) - set(self.sentencepool)
+
+                if len(sentence_candidates) > 0:
+                    # Append a random candidate to the debate's sentencepool.
+                    selected_sentence = choice(list(sentence_candidates))
+                    self.sentencepool.append(selected_sentence)
+                    self.log.append(f"Sentence {selected_sentence} added to the sentence pool.")
+
+                    # Carry along the debate stage.
+                    self.append(self[-1])
+
+                    # Now have the positions take a random stance toward the newly inserted sentence.
+                    expanded_positions = []
+                    for p in self.positions[-1]:
+                        e = deepcopy(p)
+                        # There is a 2:1 chance that the position does not suspend judgement on the
+                        # new sentence.
+                        if choice([True, True, False]):
+                            # If the positions does no suspend, there is a 1:1 chance it will assign
+                            # either truth value.
+                            e[selected_sentence] = choice([True, False])
+                        expanded_positions.append(e)
+                    
+                    self.positions.append(expanded_positions)      
+
+                else:
+                    # Failure to insert a sentence does not end the simulation, but we take note if it in the log.
+                    self.log.append("Tried to insert a new sentence to the debate but maximum extension was reached.")
 
             i += 1
             if self[-1].density() >= max_density or i >= max_steps or satisfiability_count(self[-1]) <= min_sccp:
                 break
+
         self.log.append("Simulation ended. %d steps were taken. Density at end: %f. Extension of SCCP: %d." % (i, self[-1].density(),  satisfiability_count(self[-1])))
         
         if quiet:

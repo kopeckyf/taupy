40e70098db1515e04cd8e96e5ed1fd82e84d7e29
Felix Kopecky <kopeckyf@users.noreply.github.com>
2024-12-02 15:21:52 +0100

    Renmove old `pandas` sub-module

--- taupy/data/pandas.py
+++
@@ -1,5 +0,0 @@
-from pandas import DataFrame
-
-def store_stimulation(simulation, filename="name.csv"):
-    data = DataFrame(data = {'debate': simulation, 'positions': simulation.positions})
-    data.to_csv(filename)
